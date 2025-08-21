bb3d6695ae2df136fa90d2c16d502530101f03fa
Yithian01 <Yithian01@git.com>
2025-08-21 21:31:28 +0900

    feat: create auction item function refator: casecade itemDef to itemEffect feat: devlop dto to acutionResponse for json dependency

--- src/main/java/com/scriptopia/demo/controller/AuctionController.java
+++ src/main/java/com/scriptopia/demo/controller/AuctionController.java
@@ -1,5 +1,9 @@
 package com.scriptopia.demo.controller;
 
+
+import com.scriptopia.demo.dto.auction.AuctionRequest;
+import com.scriptopia.demo.dto.auction.TradeResponse;
+import com.scriptopia.demo.dto.auction.TradeFilterRequest;
 import com.scriptopia.demo.service.AuctionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +16,22 @@ public class AuctionController {
 
     private final AuctionService auctionService;
 
-    @PostMapping
-    public ResponseEntity<String> createAuction(
-            @RequestBody com.scriptopia.demo.dto.auction.AuctionRequest requestDto,
-            @RequestHeader("token") String userId) {   // 헤더에서 userId 가져오기 임시임
 
-        return ResponseEntity.ok(auctionService.createAuction(requestDto, userId));
+    @PostMapping
+    public ResponseEntity<String> createAuction(@RequestBody AuctionRequest dto,
+                                                @RequestHeader("token") String userId ){   // 헤더에서 userId 가져오기 임시임
+
+        return ResponseEntity.ok(auctionService.createAuction(dto, userId));
     }
+
+
+    @GetMapping
+    public ResponseEntity<TradeResponse> getTrades(
+            @RequestBody TradeFilterRequest requestDto) {
+
+        TradeResponse response = auctionService.getTrades(requestDto);
+        return ResponseEntity.ok(response);
+
+    }
+
 }
