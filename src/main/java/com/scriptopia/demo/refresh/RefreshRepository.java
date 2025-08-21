4d04cb24486e40096fc5da056b7a603311149ac9
juns0720 <juns0720@naver.com>
2025-08-21 16:24:28 +0900

    feat: add RefreshRepository Interface

---
+++ src/main/java/com/scriptopia/demo/refresh/RefreshRepository.java
@@ -0,0 +1,15 @@
+package com.scriptopia.demo.refresh;
+
+
+import java.util.Optional;
+
+public interface RefreshRepository {
+    void save(RefreshSession session);
+    Optional<RefreshSession> find(long userId, String jti);
+    void delete(long userId, String jti);
+
+    Optional<RefreshSession> findByDevice(long userId, String device);
+    void deleteByDevice(long userId, String device);
+
+    void deleteAllForUsr(long userId);
+}
