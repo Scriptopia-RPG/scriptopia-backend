62871d15128704a072ebf3b87eb9bcb9ed889e70
junseo Lee <juns0720@naver.com>
2025-08-27 17:21:20 +0900

    feat: update error code notation

--- src/main/java/com/scriptopia/demo/exception/ErrorCode.java
+++ src/main/java/com/scriptopia/demo/exception/ErrorCode.java
@@ -10,18 +10,18 @@ public enum ErrorCode {
 
 
     //400 Bad Request
-    E_400("E_400", "잘못된 요청 형식입니다.", HttpStatus.BAD_REQUEST),
-    E_400_MISSING_EMAIL("E_400_EMAIL_REQUIRED", "이메일은 필수 입력 값입니다.", HttpStatus.BAD_REQUEST),
-    E_400_INVALID_EMAIL_FORMAT("E_400_EMAIL_INVALID","이메일 형식이 올바르지 않습니다.",HttpStatus.BAD_REQUEST),
+    E_400("E400000", "잘못된 요청 형식입니다.", HttpStatus.BAD_REQUEST),
+    E_400_MISSING_EMAIL("E400001", "이메일은 필수 입력 값입니다.", HttpStatus.BAD_REQUEST),
+    E_400_INVALID_EMAIL_FORMAT("E400002","이메일 형식이 올바르지 않습니다.",HttpStatus.BAD_REQUEST),
 
     //401 Unauthorized
-    E_401_INVALID_CREDENTIALS("E_401_CRED","이메일 또는 비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
+    E_401_INVALID_CREDENTIALS("E401001","이메일 또는 비밀번호가 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
 
     //403 Forbidden
-    E_403_ROLE_FORBIDDEN("E_403_ROLE", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
+    E_403_ROLE_FORBIDDEN("E403001", "접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
 
     //500 INTERNAL_SERVER_ERROR
-    E_500("E_500", "예상치 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    E_500("E_500000", "예상치 못한 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     private final String code;
     private final String message;
     private final HttpStatus status;
