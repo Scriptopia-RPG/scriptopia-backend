9f51e75caf6e26081bf70b235eee8e6b7dfcefea
KII1ua <sung25480@naver.com>
2025-08-29 00:20:28 +0900

    feature/63-history-controller test complete

--- src/main/java/com/scriptopia/demo/controller/UserHistoryController.java
+++ src/main/java/com/scriptopia/demo/controller/UserHistoryController.java
@@ -5,20 +5,23 @@ import com.scriptopia.demo.dto.history.HistoryResponse;
 import com.scriptopia.demo.service.HistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/public")
 @RequiredArgsConstructor
 public class UserHistoryController {
     private final HistoryService historyService;
 
     @GetMapping("/history")
-    public ResponseEntity<List<HistoryPageResponse>> getHistory(@RequestHeader("X-User-ID") Long userId,
-                                                                @RequestParam(required = false) Long lastId,
-                                                                @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<List<HistoryPageResponse>> getHistory(@RequestParam(required = false) Long lastId,
+                                                                @RequestParam(defaultValue = "10") int size,
+                                                                @RequestParam Long userId) {
+//        Long userId = Long.valueOf(authentication.getName());
+
         return historyService.fetchMyHisotry(userId, lastId, size);
     }
 }
