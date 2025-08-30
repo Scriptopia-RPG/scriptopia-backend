07d47d36cf4589ae3a3b2bf9251a61c2f6e06692
Yithian01 <Yithian01@git.com>
2025-08-30 20:18:44 +0900

    create PiaShopController

---
+++ src/main/java/com/scriptopia/demo/controller/PiaShopController.java
@@ -0,0 +1,17 @@
+package com.scriptopia.demo.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PiaShopController {
+
+
+
+
+    @PostMapping("/admin/items/pia")
+
+}
