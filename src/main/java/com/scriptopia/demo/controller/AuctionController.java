a36d2d580f616fc4aa4c780d97f170555dc54e24
Yithian01 <Yithian01@git.com>
2025-08-30 18:17:17 +0900

    completet postman api test

--- src/main/java/com/scriptopia/demo/controller/AuctionController.java
+++ src/main/java/com/scriptopia/demo/controller/AuctionController.java
@@ -70,8 +70,6 @@ public class AuctionController {
     }
 
 
-
-
     @GetMapping("/user/trades/me")
     public ResponseEntity<MySaleItemResponse> mySaleItems(
             @RequestBody MySaleItemRequest requestDto,
