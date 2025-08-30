51ea48794c924b074f4993ca87fed8a51546f593
KII1ua <sung25480@naver.com>
2025-08-31 01:30:08 +0900

    feature/myshared-game add

---
+++ src/main/java/com/scriptopia/demo/dto/sharedgame/MySharedGameResponse.java
@@ -0,0 +1,26 @@
+package com.scriptopia.demo.dto.sharedgame;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class MySharedGameResponse {
+    private String thumbnailUrl;
+    private Long totalPlayed;
+    private String title;
+    private String worldView;
+    private String backgroundStory;
+    private LocalDateTime sharedAt;
+    private List<TagDto> tags;
+
+    @Data
+    public static class TagDto {
+        private String tagName;
+
+        public TagDto(String tagName) {
+            this.tagName = tagName;
+        }
+    }
+}
--- src/main/java/com/scriptopia/demo/repository/GameTagRepository.java
+++ src/main/java/com/scriptopia/demo/repository/GameTagRepository.java
@@ -1,6 +1,8 @@
 package com.scriptopia.demo.repository;
 
 import com.scriptopia.demo.domain.GameTag;
+import com.scriptopia.demo.dto.TagDef.TagDefCreateRequest;
+import com.scriptopia.demo.dto.sharedgame.MySharedGameResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,8 @@ public interface GameTagRepository extends JpaRepository<GameTag, Long> {
             "from GameTag gt " +
             "where gt.sharedGame.id = :sharedGameId")
     List<String> findTagNamesBySharedGameId(@Param("sharedGameId") Long sharedGameId);
+
+    @Query("select new com.scriptopia.demo.dto.TagDef.TagDefCreateRequest(gt.tagDef.tagName) " +
+            "from GameTag gt where gt.sharedGame.id = :sharedGameId")
+    List<TagDefCreateRequest> findTagsBySharedGameId(@Param("sharedGameId") Long sharedGameId);
 }
