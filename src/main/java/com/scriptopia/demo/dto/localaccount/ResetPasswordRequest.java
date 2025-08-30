07697eb9967dad8f49fc9c019ea738cf6acf3405
junseo Lee <juns0720@naver.com>
2025-08-31 04:09:04 +0900

    feat: add ResetPasswordRequest dto for reset password

---
+++ src/main/java/com/scriptopia/demo/dto/localaccount/ResetPasswordRequest.java
@@ -0,0 +1,24 @@
+package com.scriptopia.demo.dto.localaccount;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+
+    private String token;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자리여야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/]).+$",
+            message = "비밀번호는 소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
+    private String newPassword;
+}
