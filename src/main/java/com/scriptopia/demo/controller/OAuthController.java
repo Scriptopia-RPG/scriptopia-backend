204fc96e374cf028cbab0ce958698ec4a7cd90f5
junseo Lee <juns0720@naver.com>
2025-09-01 00:38:13 +0900

    feat: create api for google social login and register

--- src/main/java/com/scriptopia/demo/controller/OAuthController.java
+++ src/main/java/com/scriptopia/demo/controller/OAuthController.java
@@ -1,12 +1,41 @@
 package com.scriptopia.demo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.scriptopia.demo.dto.localaccount.LoginResponse;
+import com.scriptopia.demo.dto.oauth.OAuthLoginResponse;
+import com.scriptopia.demo.dto.oauth.SocialSignupRequest;
+import com.scriptopia.demo.service.OAuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/public/oauth")
 @RequiredArgsConstructor
 public class OAuthController {
 
+    private final OAuthService oAuthService;
+
+    @GetMapping("/{provider}")
+    public ResponseEntity<OAuthLoginResponse> login(
+            @PathVariable("provider") String provider,
+            @RequestParam("code") String code,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws JsonProcessingException {
+        OAuthLoginResponse result = oAuthService.login(provider, code, request, response);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<OAuthLoginResponse> signup(
+            @RequestBody SocialSignupRequest req,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        OAuthLoginResponse result = oAuthService.signup(req, request, response);
+        return ResponseEntity.ok(result);
+    }
 }
