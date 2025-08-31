b52814dd108887815b0925fcc9a91dae540c7ade
junseo Lee <juns0720@naver.com>
2025-08-31 18:02:18 +0900

    feat: add OAuthUserInfo dto for google login

---
+++ src/main/java/com/scriptopia/demo/controller/OAuthController.java
@@ -0,0 +1,12 @@
+package com.scriptopia.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/oauth")
+@RequiredArgsConstructor
+public class OAuthController {
+
+}
---
+++ src/main/java/com/scriptopia/demo/dto/oauth/OAUthUserInfo.java
@@ -0,0 +1,15 @@
+package com.scriptopia.demo.dto.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OAUthUserInfo {
+    private String id;
+    private String email;
+    private String name;
+    private String profileImage;
+}
