ceb5ea3dea2904e3b371a05bfb95d4839abb3c15
junseo Lee <juns0720@naver.com>
2025-08-31 18:53:19 +0900

    feat: add dto OAuthLoginResponse

--- src/main/java/com/scriptopia/demo/dto/oauth/OAuthLoginResponse.java
+++ src/main/java/com/scriptopia/demo/dto/oauth/OAuthLoginResponse.java
@@ -1,10 +1,12 @@
 package com.scriptopia.demo.dto.oauth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OAuthLoginResponse {
