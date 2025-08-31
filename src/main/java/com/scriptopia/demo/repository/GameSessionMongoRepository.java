3bfbdebd047f0d7d4b092c2dd95b7816421ec425
Yithian01 <Yithian01@git.com>
2025-08-31 18:23:04 +0900

    feat: create GameSessionMongoRepository to repository

---
+++ src/main/java/com/scriptopia/demo/repository/GameSessionMongoRepository.java
@@ -0,0 +1,13 @@
+package com.scriptopia.demo.repository;
+
+import com.scriptopia.demo.domain.mongo.GameSessionMongo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface GameSessionMongoRepository extends MongoRepository<GameSessionMongo, String> {
+
+
+    // userId로 진행 중인 게임 조회
+    Optional<GameSessionMongo> findByUserIdAndSceneTypeNot(String userId, String sceneType);
+}
