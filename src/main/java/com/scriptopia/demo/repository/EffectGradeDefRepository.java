5db74d5b1fa16980c382160194b7d3b2d971fda5
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-09-01 23:24:57 +0900

    feat create Query method findPricebyGrade

--- src/main/java/com/scriptopia/demo/repository/EffectGradeDefRepository.java
+++ src/main/java/com/scriptopia/demo/repository/EffectGradeDefRepository.java
@@ -2,10 +2,15 @@ package com.scriptopia.demo.repository;
 
 import com.scriptopia.demo.domain.EffectGradeDef;
 import com.scriptopia.demo.domain.Grade;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface EffectGradeDefRepository extends JpaRepository<EffectGradeDef, Long> {
     Optional<EffectGradeDef> findByGrade(Grade grade);
+
+    @Query("SELECT egd.price FROM EffectGradeDef egd WHERE egd.grade = :grade")
+    Integer findPriceByGrade(@Param("grade") Grade grade);
 }
