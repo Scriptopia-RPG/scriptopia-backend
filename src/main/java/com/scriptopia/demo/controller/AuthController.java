c432602f54f562b8e878538bde95b6b1ebb23ca6
Yithian01 <Yithian01@git.com>
2025-09-01 15:37:42 +0900

    refactor: authControllerName

--- src/main/java/com/scriptopia/demo/controller/AuthController.java
+++ src/main/java/com/scriptopia/demo/controller/AuthController.java
@@ -31,38 +31,6 @@ public class AuthController {
     private static final String COOKIE_SAMESITE = "None";
 
 
-    @PatchMapping("public/auth/password/reset")
-    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
-        localAccountService.resetPassword(request.getToken(), request.getNewPassword());
-
-        return ResponseEntity.ok("비밀번호가 성공적으로 변경되었습니다.");
-    }
-
-    @PostMapping("/public/auth/password/send-link")
-    public ResponseEntity<?> sendResetMail(@Valid @RequestBody SendCodeRequest request){
-
-        localAccountService.sendResetPasswordMail(request.getEmail());
-
-        return ResponseEntity.ok("비밀번호 초기화 링크를 전송했습니다.");
-    }
-
-    @PostMapping("/public/auth/verify-email")
-    public ResponseEntity<?> verifyEmail(@Valid @RequestBody VerifyEmailRequest request) {
-
-        localAccountService.verifyEmail(request);
-
-        return ResponseEntity.ok("사용 가능한 이메일입니다.");
-    }
-
-    @PostMapping("/public/auth/login")
-    public ResponseEntity<LoginResponse> login(
-            @RequestBody @Valid LoginRequest req,
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) {
-
-        return ResponseEntity.ok(localAccountService.login(req, request, response));
-    }
 
     @PostMapping("/user/auth/logout")
     public ResponseEntity<?> logout(
@@ -76,6 +44,17 @@ public class AuthController {
         return ResponseEntity.ok("로그아웃 되었습니다.");
     }
 
+
+    @PostMapping("/public/auth/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody @Valid LoginRequest req,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        return ResponseEntity.ok(localAccountService.login(req, request, response));
+    }
+
     @PostMapping("/public/auth/register")
     public ResponseEntity<?> register(
             @RequestBody @Valid RegisterRequest request
@@ -84,19 +63,44 @@ public class AuthController {
         return ResponseEntity.ok("회원가입에 성공했습니다.");
     }
 
-    @PostMapping("/public/auth/send-code")
+    @PostMapping("/public/auth/email/verify")
+    public ResponseEntity<?> verifyEmail(@Valid @RequestBody VerifyEmailRequest request) {
+
+        localAccountService.verifyEmail(request);
+
+        return ResponseEntity.ok("사용 가능한 이메일입니다.");
+    }
+
+
+    @PostMapping("/public/auth/email/code/send")
     public ResponseEntity<String> sendCode(@RequestBody @Valid SendCodeRequest request) {
         localAccountService.sendVerificationCode(request.getEmail());
         return ResponseEntity.ok("인증 코드가 이메일로 발송되었습니다.");
     }
 
-    @PostMapping("/public/auth/verify-code")
+    @PostMapping("/public/auth/email/code/verify")
     public ResponseEntity<String> verifyCode(@RequestBody @Valid VerifyCodeRequest request) {
         localAccountService.verifyCode(request.getEmail(), request.getCode());
         return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
 
     }
 
+
+    @PostMapping("/public/auth/password/reset-link/send")
+    public ResponseEntity<?> sendResetMail(@Valid @RequestBody SendCodeRequest request){
+
+        localAccountService.sendResetPasswordMail(request.getEmail());
+
+        return ResponseEntity.ok("비밀번호 초기화 링크를 전송했습니다.");
+    }
+
+
+    @PatchMapping("public/auth/password/reset")
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest request) {
+        localAccountService.resetPassword(request.getToken(), request.getNewPassword());
+
+        return ResponseEntity.ok("비밀번호가 성공적으로 변경되었습니다.");
+    }
 
 
     @PatchMapping("/user/auth/password/change")
