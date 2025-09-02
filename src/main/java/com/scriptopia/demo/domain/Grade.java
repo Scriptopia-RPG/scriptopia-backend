87e44cd309bc644b1fd669a34f3260e700fd98f6
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-09-02 16:30:16 +0900

    feat: update Grade enum class > mapping enum with attackPower,armorPower

--- src/main/java/com/scriptopia/demo/domain/Grade.java
+++ src/main/java/com/scriptopia/demo/domain/Grade.java
@@ -1,9 +1,26 @@
 package com.scriptopia.demo.domain;
 
 public enum Grade {
-    COMMON,
-    UNCOMMON,
-    RARE,
-    EPIC,
-    LEGENDARY
+    COMMON(30, 137),
+    UNCOMMON(35, 177),
+    RARE(40, 216),
+    EPIC(45, 260),
+    LEGENDARY(50, 312);
+
+    private final int attackPower;   // 무기 공격력
+    private final int defensePower;  // 방어구 방어력
+
+    Grade(int attackPower, int defensePower) {
+        this.attackPower = attackPower;
+        this.defensePower = defensePower;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public int getDefensePower() {
+        return defensePower;
+    }
+
 }
