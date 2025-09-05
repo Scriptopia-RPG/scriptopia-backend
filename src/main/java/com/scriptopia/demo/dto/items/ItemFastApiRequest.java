8566f5d16ec50a5bd5886070cfe4ec55252ed740
Yithian01 <Yithian01@git.com>
2025-09-06 04:20:14 +0900

    refactor: change each dto

--- src/main/java/com/scriptopia/demo/dto/items/ItemFastApiRequest.java
+++ src/main/java/com/scriptopia/demo/dto/items/ItemFastApiRequest.java
@@ -1,8 +1,12 @@
 package com.scriptopia.demo.dto.items;
 
+import com.scriptopia.demo.domain.Grade;
 import com.scriptopia.demo.domain.ItemType;
+import com.scriptopia.demo.domain.Stat;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 
 @Data
@@ -12,6 +16,15 @@ public class ItemFastApiRequest {
     private String worldView;
     private String location;
     private ItemType category;
+    private int baseStat;
+    private Stat mainStat;
+    private Grade grade;
+    private List<Grade> itemEffect;
+    private int strength;
+    private int agility;
+    private int intelligence;
+    private int luck;
+    private Long price;
     private String playerTrait;
     private String previousStory;
 
