49775e710747ad61a7000f3c02000c0f1e75f02d
Yithian01 <Yithian01@git.com>
2025-09-06 03:01:23 +0900

    feat: create ChoiceEventType domain

--- src/main/java/com/scriptopia/demo/domain/ChoiceEventType.java
+++ src/main/java/com/scriptopia/demo/domain/ChoiceEventType.java
@@ -1,5 +1,36 @@
 package com.scriptopia.demo.domain;
 
+import lombok.Getter;
+
+import java.security.SecureRandom;
+
+
+@Getter
 public enum ChoiceEventType {
-    LIVING, NONLIVING
+    LIVING(50),
+    NONLIVING(50);
+
+
+    private final int ChoiceEventChance;
+
+
+    private static final SecureRandom random = new SecureRandom();
+
+
+    ChoiceEventType(final int ChoiceEventChance) {
+        this.ChoiceEventChance = ChoiceEventChance;
+    }
+
+    public ChoiceEventType getChoiceEventType() {
+        int rand = random.nextInt(100) + 1;
+        int cumulative = 0;
+
+        for (ChoiceEventType Choicetype : ChoiceEventType.values()) {
+            cumulative += Choicetype.getChoiceEventChance();
+            if (cumulative >= rand) {
+                return Choicetype;
+            }
+        }
+        return NONLIVING;
+    }
 }
