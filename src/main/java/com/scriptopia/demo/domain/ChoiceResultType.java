528a982b87c4dd4a592b2933015332e9a7fb513b
Yithian01 <Yithian01@git.com>
2025-09-06 03:07:43 +0900

    refactor: get random ChoiceType

--- src/main/java/com/scriptopia/demo/domain/ChoiceResultType.java
+++ src/main/java/com/scriptopia/demo/domain/ChoiceResultType.java
@@ -1,5 +1,31 @@
 package com.scriptopia.demo.domain;
 
+import java.security.SecureRandom;
+
 public enum ChoiceResultType {
-    BATTLE, REWARD, SHOP, NONE
+    BATTLE(40),
+    CHOICE(30),
+    SHOP(10),
+    NONE(50);
+
+    private int nextEventType;
+
+    private static final SecureRandom random = new SecureRandom();
+
+    ChoiceResultType(int nextEventType) {
+        this.nextEventType = nextEventType;
+    }
+
+
+    public ChoiceResultType nextResultType() {
+        int rand = random.nextInt(nextEventType);
+        int cumulative = 0;
+
+        for(ChoiceResultType type : values()) {
+            if(rand == type.nextEventType) {
+                return type;
+            }
+        }
+        return NONE;
+    }
 }
