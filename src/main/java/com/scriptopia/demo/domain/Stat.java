07f29abef9bdffb1ab0f18424a337f099a642b5d
Yithian01 <Yithian01@git.com>
2025-09-05 22:00:54 +0900

    feat: create getRandomStat becuase high cohesion

--- src/main/java/com/scriptopia/demo/domain/Stat.java
+++ src/main/java/com/scriptopia/demo/domain/Stat.java
@@ -2,9 +2,24 @@ package com.scriptopia.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.security.SecureRandom;
+
 public enum Stat {
     INTELLIGENCE,
     STRENGTH,
     AGILITY,
-    LUCK
+    LUCK;
+
+
+
+    private static final SecureRandom random = new SecureRandom();
+
+    /**
+     * 무작위로 하나의 스탯을 반환
+     */
+    public static Stat getRandomStat() {
+        Stat[] values = Stat.values();
+        return values[random.nextInt(values.length)];
+    }
+
 }
