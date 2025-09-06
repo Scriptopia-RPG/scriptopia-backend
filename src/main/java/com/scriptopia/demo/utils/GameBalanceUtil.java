a14035657b1e366ea65e8c0ef5a3b2f157573354
Yithian01 <Yithian01@git.com>
2025-09-06 13:57:14 +0900

    feat: create method to getRandomItemStatByGrade

--- src/main/java/com/scriptopia/demo/utils/GameBalanceUtil.java
+++ src/main/java/com/scriptopia/demo/utils/GameBalanceUtil.java
@@ -24,4 +24,31 @@ public class GameBalanceUtil {
         }
         return stats;
     }
+
+    public static int[] getRandomItemStatsByGrade(Grade grade) {
+        int min = 0, max = 0;
+        switch (grade) {
+            case COMMON -> { min = 0; max = 3; }
+            case UNCOMMON -> { min = 1; max = 4; }
+            case RARE -> { min = 2; max = 5; }
+            case EPIC -> { min = 4; max = 7; }
+            case LEGENDARY -> { min = 5; max = 8; }
+        }
+
+        // 총합 랜덤 결정
+        int totalPoints = secureRandom.nextInt(max - min + 1) + min;
+
+        // 배열 생성 (0: STR, 1: AGI, 2: INT, 3: LUCK)
+        int[] stats = new int[4];
+
+        // 랜덤 분배
+        for (int i = 0; i < totalPoints; i++) {
+            stats[secureRandom.nextInt(4)]++;
+        }
+
+        return stats;
+    }
+
+
+
 }
