91faa6badedff41f52263aa5114767fc15579c83
Yithian01 <Yithian01@git.com>
2025-09-06 20:36:59 +0900

    feat: create item and save mongo, rdb

--- src/main/java/com/scriptopia/demo/controller/ItemController.java
+++ src/main/java/com/scriptopia/demo/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.scriptopia.demo.controller;
 
 import com.scriptopia.demo.dto.develop.ItemDefResponse;
 import com.scriptopia.demo.dto.items.ItemDefRequest;
+import com.scriptopia.demo.dto.items.ItemFastApiResponse;
 import com.scriptopia.demo.service.ItemDefService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,8 @@ public class ItemController {
 
 
     @PostMapping
-    public ResponseEntity<ItemDefResponse> createItem(@RequestBody ItemDefRequest request) {
-        ItemDefResponse savedItem = itemDefService.createItem(request);
+    public ResponseEntity<ItemFastApiResponse> createItem(@RequestBody ItemDefRequest request) {
+        ItemFastApiResponse savedItem = itemDefService.createItem(request);
         return ResponseEntity.ok(savedItem);
     }
 
--- src/main/java/com/scriptopia/demo/service/ItemDefService.java
+++ src/main/java/com/scriptopia/demo/service/ItemDefService.java
@@ -1,17 +1,25 @@
 package com.scriptopia.demo.service;
 
+import com.scriptopia.demo.adapter.EffectAdapter;
 import com.scriptopia.demo.domain.*;
+import com.scriptopia.demo.domain.mongo.ItemDefMongo;
+import com.scriptopia.demo.domain.mongo.ItemEffectMongo;
 import com.scriptopia.demo.dto.develop.ItemDefResponse;
 import com.scriptopia.demo.dto.develop.ItemEffectResponse;
 import com.scriptopia.demo.dto.items.*;
 import com.scriptopia.demo.repository.EffectGradeDefRepository;
 import com.scriptopia.demo.repository.ItemDefRepository;
 import com.scriptopia.demo.repository.ItemGradeDefRepository;
+import com.scriptopia.demo.repository.mongo.ItemDefMongoRepository;
+import com.scriptopia.demo.utils.GameBalanceUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,9 +31,11 @@ public class ItemDefService {
     private final ItemDefRepository itemDefRepository;
     private final ItemGradeDefRepository itemGradeDefRepository;
     private final EffectGradeDefRepository effectGradeDefRepository;
+    private final ItemDefMongoRepository itemDefMongoRepository;
 
-    @Transactional
-    public ItemDefResponse createItem(ItemDefRequest request) {
+
+    @Transactional(readOnly = false)
+    public ItemFastApiResponse createItem(ItemDefRequest request) {
         /**
          * 1. 카테고리
          * 2. 등급
@@ -34,27 +44,133 @@ public class ItemDefService {
          * 5. 아이템 이펙트( 최대 등급 3개)
          * 6. 추가 스탯
          */
+        ItemType itemCategory = ItemType.getRandomItemType();
+        Grade itemGrade = Grade.getRandomGradeByProbability();
+        int baseStat = Grade.getRandomBaseStat(itemCategory, itemGrade);
+        Stat mainStat = Stat.getRandomMainStat();
+        int[] additionalStats = GameBalanceUtil.getRandomItemStatsByGrade(itemGrade); // strength, agility, intelligence, luck
+
+
+        List<EffectProbability> effectGrades = new ArrayList<>();
+        List<Long> effectGradesList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            EffectProbability effectGrade = EffectProbability.getRandomEffectGradeByWeaponGrade(itemGrade);
+            if (effectGrade != null) {
+                effectGrades.add(effectGrade);
+                effectGradesList.add(effectGradeDefRepository.findPriceByGrade(effectGrade));
+            }
+        }
+
+        Long gradeGradePrice = itemGradeDefRepository.findPriceByGrade(itemGrade);
+        Long itemPrice = GameBalanceUtil.getItemPriceByGrade(gradeGradePrice, effectGradesList);
+
+
+
 
 
         ItemFastApiRequest fastRequest = ItemFastApiRequest.builder()
                 .worldView(request.getWorldView())
                 .location(request.getLocation())
-                .category(ItemType.getRandomItemType())
-                .baseStat()
-                .mainStat(Stat.getRandomMainStat())
-                .grade(Grade.RARE)
-                .itemEffect(List.of(Grade.COMMON, Grade.UNCOMMON))
-                .strength(10)
-                .agility(5)
-                .intelligence(3)
-                .luck(2)
-                .price(1000L)
-                .playerTrait("용맹함")
-                .previousStory("고대의 유산에서 발견됨")
+                .category(itemCategory)
+                .baseStat(baseStat)
+                .mainStat(mainStat)
+                .grade(itemGrade)
+                .itemEffect(effectGrades)
+                .strength(additionalStats[0])
+                .agility(additionalStats[1])
+                .intelligence(additionalStats[2])
+                .luck(additionalStats[3])
+                .price(itemPrice)
+                .playerTrait(request.getPlayerTrait())
+                .previousStory(request.getPreviousStory())
                 .build();
 
 
+        // 추후 config 에서 통합관리
+        WebClient client = WebClient.builder()
+                .baseUrl("http://localhost:8000") // FastAPI 서버 주소
+                .build();
+
+
+        ItemFastApiResponse response = client.post()
+                .uri("/games/item") // FastAPI 엔드포인트
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(fastRequest) // 생성한 ItemFastApiRequest 객체
+                .retrieve()
+                .bodyToMono(ItemFastApiResponse.class)
+                .block(); // 블로킹 호출 (간단 테스트용)
+
+        System.out.println(response );
+
+
+        List<ItemEffectMongo> mongoEffects = new ArrayList<>();
+        List<ItemFastApiResponse.ItemEffect> apiEffects = response.getItemEffect();
+
+        for (int i = 0; i < apiEffects.size(); i++) {
+            ItemFastApiResponse.ItemEffect apiEffect = apiEffects.get(i);
+            EffectProbability effectGrade = i < effectGrades.size() ? effectGrades.get(i) : null;
+
+            mongoEffects.add(ItemEffectMongo.builder()
+                    .grade(effectGrade != null ? EffectAdapter.toGrade(effectGrade) : Grade.COMMON)
+                    .itemEffectName(apiEffect.getItemEffectName())
+                    .itemEffectDescription(apiEffect.getItemEffectDescription())
+                    .build());
+        }
+
+        System.out.println("mongoEffects = " + mongoEffects );
+
+
+        ItemDefMongo itemDefMongo = ItemDefMongo.builder()
+                .itemPicSrc("test link")
+                .name(response.getItemName())
+                .description(response.getItemDescription())
+                .category(itemCategory)
+                .baseStat(baseStat)
+                .itemEffect(mongoEffects)
+                .strength(additionalStats[0])
+                .agility(additionalStats[1])
+                .intelligence(additionalStats[2])
+                .luck(additionalStats[3])
+                .mainStat(mainStat)
+                .grade(itemGrade)
+                .price(itemPrice)
+                .build();
+
+        itemDefMongoRepository.save(itemDefMongo);
+
+        ItemDef itemDefRdb = new ItemDef();
+        itemDefRdb.setName(itemDefMongo.getName());
+        itemDefRdb.setDescription(itemDefMongo.getDescription());
+        itemDefRdb.setItemGradeDef(itemGradeDefRepository.findByGrade(itemGrade).get());
+        itemDefRdb.setPicSrc(itemDefMongo.getItemPicSrc());
+        itemDefRdb.setItemType(itemDefMongo.getCategory());
+        itemDefRdb.setBaseStat(itemDefMongo.getBaseStat());
+        itemDefRdb.setStrength(itemDefMongo.getStrength());
+        itemDefRdb.setAgility(itemDefMongo.getAgility());
+        itemDefRdb.setIntelligence(itemDefMongo.getIntelligence());
+        itemDefRdb.setLuck(itemDefMongo.getLuck());
+        itemDefRdb.setMainStat(itemDefMongo.getMainStat());
+        itemDefRdb.setPrice(itemDefMongo.getPrice());
+        itemDefRdb.setCreatedAt(LocalDateTime.now());
+
+        List<ItemEffect> rdbEffects = new ArrayList<>();
+        for (ItemEffectMongo effectMongo : itemDefMongo.getItemEffect()) {
+            ItemEffect effect = new ItemEffect();
+            effect.setItemDef(itemDefRdb);
+            effect.setEffectName(effectMongo.getItemEffectName());
+            effect.setEffectDescription(effectMongo.getItemEffectDescription());
+            effect.setEffectGradeDef(effectGradeDefRepository.findByGrade(effectMongo.getGrade()).get());
+            rdbEffects.add(effect);
+        }
+        itemDefRdb.setItemEffects(rdbEffects);
+
+        itemDefRepository.save(itemDefRdb);
+
+        return response;
+
     }
+
 
 
 }
