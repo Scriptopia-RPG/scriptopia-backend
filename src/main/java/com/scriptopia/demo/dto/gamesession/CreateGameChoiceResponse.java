eb28227e9ab300b34094d990343472289bccc367
Yithian01 <Yithian01@git.com>
2025-09-07 20:54:35 +0900

    feat: create CreateGameChoiceResponse dto

---
+++ src/main/java/com/scriptopia/demo/dto/gamesession/CreateGameChoiceResponse.java
@@ -0,0 +1,43 @@
+package com.scriptopia.demo.dto.gamesession;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateGameChoiceResponse {
+
+    private ChoiceInfo choiceInfo;
+    private NpcInfo npcInfo;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ChoiceInfo {
+        private String story;
+        private List<ChoiceOption> choice;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ChoiceOption {
+        private String detail;
+        private String stats;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class NpcInfo {
+        private String name;
+        private Integer rank;
+        private String trait;
+        private String npcWeaponName;
+        private String npcWeaponDescription;
+    }
+}
