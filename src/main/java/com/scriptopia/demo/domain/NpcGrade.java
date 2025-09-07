8668d809f196bc9f7476c64650236045c2639cb2
Yithian01 <Yithian01@git.com>
2025-09-07 20:43:59 +0900

    refactor: add capter Npc Ramdom probabilty

--- src/main/java/com/scriptopia/demo/domain/NpcGrade.java
+++ src/main/java/com/scriptopia/demo/domain/NpcGrade.java
@@ -3,40 +3,38 @@ package com.scriptopia.demo.domain;
 import lombok.Getter;
 
 import java.security.SecureRandom;
+import java.util.List;
 
 @Getter
 public enum NpcGrade {
-    GRADE1(1,  70, 18, 14, 8, 4),
-    GRADE2(2, 105,26, 16, 9, 5),
-    GRADE3(3,  140,35, 14, 9, 6),
-    GRADE4(4,152,38, 12, 12, 7),
-    GRADE5(5, 188,47, 10, 12, 9),
-    GRADE6(6, 204,51, 9, 12 ,11),
-    GRADE7(7, 248,62, 8, 11, 13),
-    GRADE8(8, 268,67, 7, 10, 13),
-    GRADE9(9, 320,80, 6, 10 , 12),
-    GRADE10(10, 344,86, 3, 5, 7),
-    GRADE11(11, 707, 101, 1, 1, 2),
-    GRADE12(12, 963,107, 0, 1, 1);
+    GRADE1(1,  70, 18, List.of(14, 8, 4)),
+    GRADE2(2, 105, 26, List.of(16, 9, 5)),
+    GRADE3(3, 140, 35, List.of(14, 9, 6)),
+    GRADE4(4, 152, 38, List.of(12, 12, 7)),
+    GRADE5(5, 188, 47, List.of(10, 12, 9)),
+    GRADE6(6, 204, 51, List.of(9, 12, 11)),
+    GRADE7(7, 248, 62, List.of(8, 11, 13)),
+    GRADE8(8, 268, 67, List.of(7, 10, 13)),
+    GRADE9(9, 320, 80, List.of(6, 10, 12)),
+    GRADE10(10, 344, 86, List.of(3, 5, 7)),
+    GRADE11(11, 707, 101, List.of(1, 1, 2)),
+    GRADE12(12, 963, 107, List.of(0, 1, 1));
+
 
     private final int gradeNumber;
     private final int defense;
     private final int attack;
-    private final int chapter1;
-    private final int chapter2;
-    private final int chapter3;
+    private final List<Integer> chapter;
 
 
     private static final SecureRandom random = new SecureRandom();
 
     NpcGrade(int gradeNumber, int defense,
-             int attack, int chapter1, int chapter2, int chapter3) {
+             int attack, List<Integer> chapter) {
         this.gradeNumber = gradeNumber;
         this.defense = defense;
         this.attack = attack;
-        this.chapter1 = chapter1;
-        this.chapter2 = chapter2;
-        this.chapter3 = chapter3;
+        this.chapter = chapter;
     }
 
     public static NpcGrade getByGradeNumber(int gradeNumber) {
@@ -48,18 +46,21 @@ public enum NpcGrade {
         return null;
     }
 
-    public static Integer getNpcNumberByRandom (int currentChapter) {
+    public static Integer getNpcNumberByRandom(int currentChapter) {
         int rand = random.nextInt(100) + 1; // 1~100
         int cumulative = 0;
 
-        for (NpcGrade grade : NpcGrade..values()) {
+        for (NpcGrade grade : NpcGrade.values()) {
+            int weight = grade.getChapter().get(currentChapter - 1);
+
+            cumulative += weight;
             if (rand <= cumulative) {
-                return grade;
+                return grade.getGradeNumber();
             }
         }
-        return 12;
-    }
 
+        return NpcGrade.GRADE12.getGradeNumber();
+    }
 
 
     // ±10% 랜덤 방어력
