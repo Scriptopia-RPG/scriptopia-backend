d3060d1980b042d35372a72a9c6794f0009f037c
Yithian01 <Yithian01@git.com>
2025-09-07 16:42:06 +0900

    refactor: change method name to _Id

--- src/main/java/com/scriptopia/demo/repository/GameSessionRepository.java
+++ src/main/java/com/scriptopia/demo/repository/GameSessionRepository.java
@@ -9,13 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
-    Optional<GameSession> findByUser_IdAndMongoId(Long userId, String mongoId);
-
-    boolean existsByUser_Id(Long userId);
+    Optional<GameSession> findByUserIdAndMongoId(Long userId, String mongoId);
 
     @Query("select g from GameSession g join g.user u where u.id = :userId")
     Optional<GameSession> findByMongoId(@Param("userId") Long userId);
 
     @Query("select case when (Count(g) > 0) then true else false end from GameSession g join g.user u where u.id = :userId")
-    boolean existsByUserId(@Param("userid") Long userId);
+    boolean existsByUserId(@Param("userId") Long userId);
 }
