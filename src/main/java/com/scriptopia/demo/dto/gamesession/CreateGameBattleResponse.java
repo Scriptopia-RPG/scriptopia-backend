cac2837a420a8534ca33cc3ce941506119741ce3
Yithian01 <Yithian01@git.com>
2025-09-09 20:41:00 +0900

    feat: create CreateGameBattleResponse dto

--- src/main/java/com/scriptopia/demo/dto/gamesession/CreateGameBattleResponse.java
+++ src/main/java/com/scriptopia/demo/dto/gamesession/CreateGameBattleResponse.java
@@ -1,4 +1,21 @@
 package com.scriptopia.demo.dto.gamesession;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
 public class CreateGameBattleResponse {
+    private BattleInfoDto battleInfo;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BattleInfoDto {
+        private List<String> turnInfo; // 턴별 전투 로그
+        private String reCap;          // 전투 요약
+    }
 }
