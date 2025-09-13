085a1a949e9bb650d158fc2214219e4cb9676cf8
Yithian01 <Yithian01@git.com>
2025-09-13 17:27:42 +0900

    Add test controller for Thymeleaf experiment

--- src/main/java/com/scriptopia/demo/controller/TestEnvController.java
+++ src/main/java/com/scriptopia/demo/controller/TestEnvController.java
@@ -1,7 +1,9 @@
 package com.scriptopia.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,4 +12,5 @@ public class TestEnvController {
     public String mainPage() {
         return "index"; // templates/index.html
     }
+
 }
