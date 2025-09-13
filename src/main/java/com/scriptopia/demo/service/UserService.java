2cbedb6460a541ed1c2ccbb1ffc7a6d61a5bd2ed
junseo Lee <74139368+juns0720@users.noreply.github.com>
2025-09-13 11:50:55 +0900

    feat: add business logic for retrieving user items (UserService)

--- src/main/java/com/scriptopia/demo/service/UserService.java
+++ src/main/java/com/scriptopia/demo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.scriptopia.demo.service;
 
 import com.scriptopia.demo.domain.User;
+import com.scriptopia.demo.domain.UserPiaItem;
 import com.scriptopia.demo.domain.UserSetting;
+import com.scriptopia.demo.dto.users.PiaItemDTO;
 import com.scriptopia.demo.dto.users.UserAssetsResponse;
 import com.scriptopia.demo.dto.users.UserSettingsDTO;
 import com.scriptopia.demo.exception.CustomException;
 import com.scriptopia.demo.exception.ErrorCode;
+import com.scriptopia.demo.repository.UserPiaItemRepository;
 import com.scriptopia.demo.repository.UserRepository;
 import com.scriptopia.demo.repository.UserSettingRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +26,7 @@ public class UserService {
 
     private final UserSettingRepository userSettingRepository;
     private final UserRepository userRepository;
+    private final UserPiaItemRepository userPiaItemRepository;
 
     @Transactional
     public UserSettingsDTO getUserSettings(String userId){
@@ -57,7 +63,6 @@ public class UserService {
 
     @Transactional
     public UserAssetsResponse getUserAssets(String userId){
-
         User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(
                 () -> new CustomException(ErrorCode.E_404_USER_NOT_FOUND)
         );
@@ -66,6 +71,24 @@ public class UserService {
                 .pia(user.getPia())
                 .build();
 
+    }
+
+    @Transactional
+    public List<PiaItemDTO>  getPiaItems(String userId){
+        User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(
+                () -> new CustomException(ErrorCode.E_404_USER_NOT_FOUND)
+        );
+        List<PiaItemDTO> piaItems = new ArrayList<>();
+        for (UserPiaItem userPiaItem : userPiaItemRepository.findByUserId(Long.valueOf(userId))) {
+            piaItems.add(
+                    PiaItemDTO.builder()
+                            .name(userPiaItem.getPiaItem().getName())
+                            .quantity(userPiaItem.getQuantity())
+                            .build()
+            );
+        }
+
+        return piaItems;
     }
 
 
