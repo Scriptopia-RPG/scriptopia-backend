75d28d0663535cab842d70743582c173d8c75a8e
황성윤 <86961575+KII1ua@users.noreply.github.com>
2025-09-13 19:49:17 +0900

    Revert "[Feature] 프로필 이미지 변경"

---
+++ src/main/java/com/scriptopia/demo/controller/TagDefController.java
@@ -0,0 +1,29 @@
+package com.scriptopia.demo.controller;
+
+import com.scriptopia.demo.dto.TagDef.TagDefCreateRequest;
+import com.scriptopia.demo.dto.TagDef.TagDefDeleteRequest;
+import com.scriptopia.demo.service.TagDefService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/shared-games/tags")
+@RequiredArgsConstructor
+public class TagDefController {
+    private final TagDefService tagDefService;
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PostMapping
+    public ResponseEntity<?> addTag(@RequestBody TagDefCreateRequest req) {
+
+        return tagDefService.addTagName(req);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @DeleteMapping
+    public ResponseEntity<?> removeTag(@RequestBody TagDefDeleteRequest req) {
+        return tagDefService.removeTagName(req);
+    }
+}
--- src/main/java/com/scriptopia/demo/controller/UserCharacterImgController.java
+++ src/main/java/com/scriptopia/demo/controller/UserCharacterImgController.java
@@ -3,41 +3,23 @@ package com.scriptopia.demo.controller;
 import com.scriptopia.demo.service.UserCharacterImgService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/user/me")
+@RequestMapping("/user/img")
 @RequiredArgsConstructor
 public class UserCharacterImgController {
     private final UserCharacterImgService userCharacterImgService;
 
-    /*
-    등록할 수 있는 이미지 저장
-     */
-    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
-    @PostMapping("/save/img")
-    public ResponseEntity<?> saveCharacterImg(Authentication authentication, @RequestParam("file") MultipartFile file) {
+    @PostMapping("/save")
+    public ResponseEntity<?> saveUserCharacterImg(Authentication authentication, @RequestParam("file") MultipartFile file) {
         Long userId = Long.valueOf(authentication.getName());
 
         return userCharacterImgService.saveCharacterImg(userId, file);
-    }
-
-    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
-    @PostMapping("/profile-images/url")
-    public ResponseEntity<?> saveUserCharacterImg(Authentication authentication, @RequestParam("url") String url) {
-        Long userId = Long.valueOf(authentication.getName());
-
-        return userCharacterImgService.saveUserCharacterImg(userId, url);
-    }
-
-    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
-    @GetMapping("/images")
-    public ResponseEntity<?> getUserCharacterImgs(Authentication authentication) {
-        Long userId = Long.valueOf(authentication.getName());
-
-        return userCharacterImgService.getUserCharacterImg(userId);
     }
 }
